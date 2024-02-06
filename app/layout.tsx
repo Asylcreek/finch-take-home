5d1e91b83e2c3a1bf2cb67d3a19a788e32f90d5d
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-02-06 20:06:50 +0100

    style: Add css vars

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from 'next';
 
 import type { ReactNode } from 'react';
 
-import './base.css';
+import './styles/vars.css';
+import './styles/base.css';
+
+import { metropolis } from './helpers/fonts';
 
 export const metadata: Metadata = {
-  title: 'Homepage',
-  description: 'The homepage of your app',
+  title: 'Quickly',
+  description: 'Start turning your dream into a reality!!',
 };
 
 interface IRootLayoutProps {
@@ -15,7 +18,7 @@ interface IRootLayoutProps {
 
 export default function RootLayout({ children }: IRootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={metropolis.variable}>
       <body>{children}</body>
     </html>
   );
